2dc5a04dd1a42879848bd84f8c4889de8a1af21a
Colin Turing <colin.koga@gmail.com>
2020-02-17 15:25:58 -0700

    Refactor components now using TouchableOpacity for individ trips, TripPreview is not connected anymore will use for stack naviagtion

--- Components/TripPreview/TripPreview.js
+++ Components/TripPreview/TripPreview.js
@@ -1,18 +1,44 @@
 import React from 'react';
-import { Text, View } from 'react-native';
+import { Text, View, StyleSheet } from 'react-native';
 
 const TripPreview = ({item}) => {
   const { name, startDate, endDate, originAbbrev, finalDestinationAbbrev} = item;
 
   return (
-    <View>
-      <Text>{name}</Text>
-      <Text>{originAbbrev}</Text>
-      <Text>{finalDestinationAbbrev}</Text>
-      <Text>{startDate}</Text>
-      <Text>{endDate}</Text>
+    <View style={styles.container}>
+      <Text style={styles.name}>{name}</Text>
+      <View style={styles.cities}>
+        <Text style={styles.origin}>{originAbbrev}</Text>
+        <Text style={styles.destination}>{finalDestinationAbbrev}</Text>
+      </View>
+      <View style={styles.cities}>
+        <Text style={styles.startDate}>{startDate}</Text>
+        <Text style={styles.endDate}>{endDate}</Text>
+      </View>
     </View>
   )
 }
 
 export default TripPreview;
+
+const styles = StyleSheet.create({
+  container: {
+    borderColor: '#ddd',
+    borderWidth: 1,
+    borderRadius: 5,
+    marginTop: 10,
+    padding: 10
+  },
+  name: {
+    fontSize: 20,
+    fontWeight: 'bold',
+    alignItems: 'center',
+    margin: 'auto',
+    marginBottom: 5
+  },
+  cities: {
+    flex: 1,
+    flexDirection: 'row',
+    justifyContent: 'space-between',
+  }
+})
--- Components/Trips/Trips.js
+++ Components/Trips/Trips.js
@@ -1,120 +1,189 @@
 import React from 'react';
-import { Text, View, FlatList } from 'react-native';
+import { Text, View, FlatList, StyleSheet, ScrollView, TouchableOpacity } from 'react-native';
 import TripPreview from '../TripPreview/TripPreview';
+import { globalStyles } from '../../styles/global';
 
 const mockPreviews = [
   {
-    id: 1,
+    id: '1',
     name: 'spring break',
     startDate: '03-05-2020',
     endDate: '04-05-2020',
     origin: 'denver, co, usa',
     finalDestination: 'paris, france',
     originAbbrev: 'DEN',
-    finalDestinationAbbrev: 'PAR'
+    finalDestinationAbbrev: 'PAR',
+    description: 'Spring Break Lorem Ipsum something de lor'
   },
   {
-    id: 2,
+    id: '2',
     name: 'Summer Roadtrip',
     startDate: '06-05-2020',
     endDate: '07-05-2020',
     origin: 'denver, co, usa',
     finalDestination: 'Barcelona, spain',
     originAbbrev: 'DEN',
-    finalDestinationAbbrev: 'BRC'
+    finalDestinationAbbrev: 'BRC',
+    description: 'Summer Roadtrip Lorem Ipsum something de lor'
   },
   {
-    id: 3,
+    id: '3',
     name: 'Winter Trip',
     startDate: '11-05-2020',
     endDate: '12-05-2020',
     origin: 'denver, co, usa',
     finalDestination: 'Tokyo, Japan',
     originAbbrev: 'DEN',
-    finalDestinationAbbrev: 'TOK'
+    finalDestinationAbbrev: 'TOK',
+    description: ' Winter Lorem Ipsum something de lor'
   },
   {
-    id: 3,
+    id: '4',
     name: 'Winter Trip',
     startDate: '11-05-2020',
     endDate: '12-05-2020',
     origin: 'denver, co, usa',
     finalDestination: 'Tokyo, Japan',
     originAbbrev: 'DEN',
-    finalDestinationAbbrev: 'TOK'
+    finalDestinationAbbrev: 'TOK',
+    description: 'Lorem Ipsum something de lor'
   },
   {
-    id: 3,
+    id: '5',
     name: 'Winter Trip',
     startDate: '11-05-2020',
     endDate: '12-05-2020',
     origin: 'denver, co, usa',
     finalDestination: 'Tokyo, Japan',
     originAbbrev: 'DEN',
-    finalDestinationAbbrev: 'TOK'
+    finalDestinationAbbrev: 'TOK',
+    description: 'Lorem Ipsum something de lor'
   },
   {
-    id: 3,
+    id: '6',
     name: 'Winter Trip',
     startDate: '11-05-2020',
     endDate: '12-05-2020',
     origin: 'denver, co, usa',
     finalDestination: 'Tokyo, Japan',
     originAbbrev: 'DEN',
-    finalDestinationAbbrev: 'TOK'
+    finalDestinationAbbrev: 'TOK',
+    description: 'Lorem Ipsum something de lor'
   },
   {
-    id: 3,
+    id: '7',
     name: 'Winter Trip',
     startDate: '11-05-2020',
     endDate: '12-05-2020',
     origin: 'denver, co, usa',
     finalDestination: 'Tokyo, Japan',
     originAbbrev: 'DEN',
-    finalDestinationAbbrev: 'TOK'
+    finalDestinationAbbrev: 'TOK',
+    description: 'Lorem Ipsum something de lor'
   },
   {
-    id: 3,
+    id: '8',
     name: 'Winter Trip',
     startDate: '11-05-2020',
     endDate: '12-05-2020',
     origin: 'denver, co, usa',
     finalDestination: 'Tokyo, Japan',
     originAbbrev: 'DEN',
-    finalDestinationAbbrev: 'TOK'
+    finalDestinationAbbrev: 'TOK',
+    description: 'Lorem Ipsum something de lor'
   },
   {
-    id: 3,
+    id: '9',
     name: 'Winter Trip',
     startDate: '11-05-2020',
     endDate: '12-05-2020',
     origin: 'denver, co, usa',
     finalDestination: 'Tokyo, Japan',
     originAbbrev: 'DEN',
-    finalDestinationAbbrev: 'TOK'
+    finalDestinationAbbrev: 'TOK',
+    description: 'Lorem Ipsum something de lor'
   },
   {
-    id: 3,
+    id: '10',
     name: 'Winter Trip',
     startDate: '11-05-2020',
     endDate: '12-05-2020',
     origin: 'denver, co, usa',
     finalDestination: 'Tokyo, Japan',
     originAbbrev: 'DEN',
-    finalDestinationAbbrev: 'TOK'
+    finalDestinationAbbrev: 'TOK',
+    description: 'Lorem Ipsum something de lor'
   }
 ];
 
 const Trips = () => {
   return (
-    <View>
-      <Text>hey</Text>
-      <FlatList 
+    <View style={globalStyles.container}>
+      <ScrollView>
+        {mockPreviews.map(item => {
+          return (
+            <View key={item.id}>
+              <TouchableOpacity>
+                <View style={styles.container}>
+                  <Text style={styles.name}>{item.name}</Text>
+                  <View style={styles.cities}>
+                    <Text style={styles.origin}>{item.originAbbrev}</Text>
+                    <Text style={styles.destination}>{item.finalDestinationAbbrev}</Text>
+                  </View>
+                  <View style={styles.cities}>
+                    <Text style={styles.startDate}>{item.startDate}</Text>
+                    <Text style={styles.endDate}>{item.endDate}</Text>
+                  </View>
+                </View>
+              </TouchableOpacity>
+            </View>
+          )
+        })}
+      </ScrollView>
+
+      {/* struggling to make FL work using ScrollVIew for now, not aware of the nuiances right now but it works */}
+      {/* <FlatList 
         data={mockPreviews}
-        renderItem={({item}) => (<TripPreview item={item} key={item.id} />)}
-      />
+        renderItem={(item) => (
+          <TouchableOpacity>
+            <View style={styles.container}>
+              <Text style={styles.name}>{item.name}</Text>
+              <View style={styles.cities}>
+                <Text style={styles.origin}>{item.originAbbrev}</Text>
+                <Text style={styles.destination}>{item.finalDestinationAbbrev}</Text>
+              </View>
+              <View style={styles.cities}>
+                <Text style={styles.startDate}>{item.startDate}</Text>
+                <Text style={styles.endDate}>{item.endDate}</Text>
+              </View>
+            </View>
+          </TouchableOpacity>
+        )}
+      /> */}
     </View>
   )
 }
 
 export default Trips;
+
+const styles = StyleSheet.create({
+  container: {
+    borderColor: '#ddd',
+    borderWidth: 1,
+    borderRadius: 5,
+    marginTop: 10,
+    padding: 10
+  },
+  name: {
+    fontSize: 20,
+    fontWeight: 'bold',
+    alignItems: 'center',
+    margin: 'auto',
+    marginBottom: 5
+  },
+  cities: {
+    flex: 1,
+    flexDirection: 'row',
+    justifyContent: 'space-between',
+  }
+})
