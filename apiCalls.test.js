f848b5ca4fa2e5140d7b2ea3fd97add648492e7e
Alan Turing <austeyd@gmail.com>
2020-02-18 17:50:05 -0700

    Add tests for postNewTrip

--- apiCalls.test.js
+++ apiCalls.test.js
@@ -1,35 +1,168 @@
-import { getAllTrips } from './apiCalls';
+import { getAllTrips, postNewTrip } from './apiCalls';
 
-//mockresponses need to be polished in case BE format is misrepresented
+//mockresponses may need to be polished in case BE format is misrepresented
 
 // describe('apiCalls', () => {
 //
 //   describe('getTrips', () => {
-//     let mockResponse;
+//     let mockResponse, mockOptions, mockTrip;
 //
 //     beforeEach(() => {
+//       mockTrip = {
+//         id: '1',
+//         name: ' Spring Break',
+//         origin: 'Denver, CO, USA',
+//         originAbbrev: 'DEN',
+//         originLat: '39.7392',
+//         originLong: '104.9903',
+//         startData: '2020-03-16',
+//         endDate: '2020-03-23',
+//         user: {
+//           id: '1',
+//           name: 'John',
+//           email: 'John@gmail.com'
+//         },
+//         destination: [
+//           {
+//
+//           }
+//         ]
+//       };
+//
+//       mockOptions = {
+//         method: 'POST',
+//         body: JSON.stringify(mockTrip),
+//         headers: {
+//           'Content-Type': 'application/json',
+//           'api_key': 'b9aead4b955bccb5c57ef830580f3de5',
+//         }
+//       };
+//
 //       mockResponse = {
 //         allTrips: [
 //           {
-//             id: 1,
+//             id: '1',
 //             name: ' Spring Break',
-//             origin: ,
-//             originAbbrev: ,
-//             originLat: ,
-//             originLong: ,
-//             startData: ,
-//             endDate: ,
+//             origin: 'Denver, CO, USA',
+//             originAbbrev: 'DEN',
+//             originLat: '39.7392',
+//             originLong: '104.9903',
+//             startData: '2020-03-16',
+//             endDate: '2020-03-23',
 //             user: {
-//               id: 1,
+//               id: '1',
 //               name: 'John',
 //               email: 'John@gmail.com'
 //             },
-//             destinations: [
+//             destination: [
 //               {
-//                 location: 'Denver',
-//                 abbrev: 'DEN',
-//                 lat: '39.7392° N',
-//                 long: '104.9903° W'
+//
+//               }
+//             ]
+//           },
+//         ]
+//       };
+//       window.fetch = jest.fn().mockImplementation(() => {
+//         return Promise.resolve({
+//           ok: true,
+//           json: () => Promise.resolve(mockResponse)
+//         });
+//       });
+//     });
+//
+//     it('should call fetch with the correct URL & options', () => {
+//       getAllTrips(mockTrip);
+//
+//       expect(window.fetch).toHaveBeenCalledWith('', mockOptions);
+//     });
+//
+//     it('should return an object containing all trip objects', () => {
+//       expect(getAllTrips(mockTrip)).resolves.toEqual(mockResponse);
+//     });
+//
+//     it('should return an error message if Promise is rejected', () => {
+//       window.fetch = jest.fn().mockImplementation(() => {
+//         return Promise.resolve({ ok: false });
+//       });
+//
+//       expect(getAllTrips(mockTrip)).rejects.toEqual(Error('error retrieving trips data'));
+//     });
+//   });
+//
+//   describe('postNewTrip', () => {
+//     let mockResponse, mockOptions, mockTrip;
+//
+//     beforeEach(() => {
+//
+//       mockTrip = {
+//         id: '2',
+//         name: ' Summer Break',
+//         origin: 'Fort Collins, CO, USA',
+//         originAbbrev: 'DEN',
+//         originLat: '1',
+//         originLong: '2',
+//         startData: '3',
+//         endDate: '4',
+//         user: {
+//           id: '1',
+//           name: 'John',
+//           email: 'John@gmail.com'
+//         },
+//         destination: [
+//           {
+//
+//           }
+//         ]
+//       };
+//
+//       mockOptions = {
+//         method: 'POST',
+//         body: JSON.stringify(mockTrip),
+//         headers: {
+//           'Content-Type': 'application/json',
+//           'api_key': 'b9aead4b955bccb5c57ef830580f3de5',
+//         }
+//       };
+//
+//       mockResponse = {
+//         allTrips: [
+//           {
+//             id: '1',
+//             name: ' Spring Break',
+//             origin: 'Denver, CO, USA',
+//             originAbbrev: 'DEN',
+//             originLat: '39.7392',
+//             originLong: '104.9903',
+//             startData: '2020-03-16',
+//             endDate: '2020-03-23',
+//             user: {
+//               id: '1',
+//               name: 'John',
+//               email: 'John@gmail.com'
+//             },
+//             destination: [
+//               {
+//
+//               }
+//             ]
+//           },
+//           {
+//             id: '2',
+//             name: ' Summer Break',
+//             origin: 'Fort Collins, CO, USA',
+//             originAbbrev: 'DEN',
+//             originLat: '1',
+//             originLong: '2',
+//             startData: '3',
+//             endDate: '4',
+//             user: {
+//               id: '1',
+//               name: 'John',
+//               email: 'John@gmail.com'
+//             },
+//             destination: [
+//               {
+//
 //               }
 //             ]
 //           }
@@ -43,14 +176,14 @@ import { getAllTrips } from './apiCalls';
 //       });
 //     });
 //
-//     it('should call fetch with the correct URL', () => {
-//       getAllTrips();
+//     it('should call fetch with the correct URL & options', () => {
+//       postNewTrip(mockTrip);
 //
-//       expect(window.fetch).toHaveBeenCalledWith('');
+//       expect(window.fetch).toHaveBeenCalledWith('', mockOptions);
 //     });
 //
 //     it('should return an array of trip objects', () => {
-//       expect(getAllTrips()).resolves.toEqual(mockResponse);
+//       expect(postNewTrip(mockTrip)).resolves.toEqual(mockResponse);
 //     });
 //
 //     it('should return an error message if Promise is rejected', () => {
@@ -58,7 +191,7 @@ import { getAllTrips } from './apiCalls';
 //         return Promise.resolve({ ok: false });
 //       });
 //
-//       expect(getAllTrips()).rejects.toEqual(Error('error retrieving trips data'));
+//       expect(postNewTrip()).rejects.toEqual(Error('error posting new trip'));
 //     });
 //   });
 // });
