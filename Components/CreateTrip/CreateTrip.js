7526ae03571c0afc3f9997af6939b3a9d0e83feb
sertmer <scottertmer@gmail.com>
2020-02-23 12:15:52 -0700

    Remove inputs for restructuring

--- Components/CreateTrip/CreateTrip.js
+++ Components/CreateTrip/CreateTrip.js
@@ -4,7 +4,7 @@ import { postNewTrip } from '../../apiCalls';
 
 export const CreateTrip = ({ navigation }) => {
   return (
-    <ScrollView 
+    <ScrollView
       contentContainerStyle={styles.container}
       keyboardShouldPersistTaps='handled'
     >
@@ -16,80 +16,59 @@ export const CreateTrip = ({ navigation }) => {
       <View style={styles.inputContainer}>
         <TextInput style={styles.input} />
       </View>
-      <Text style={styles.label}>Final Location</Text>
-      <View style={styles.inputContainer}>
-        <TextInput style={styles.input} />
-      </View>
       <TouchableOpacity activeOpacity={.8} style={styles.addDestination}>
-        <Text style={{color: '#0D47A1', fontSize: 20, fontWeight: 'bold'}}>Add A Destination</Text>
-      </TouchableOpacity>
-      <TouchableOpacity activeOpacity={.8} style={styles.submitDestination}>
-        <Text style={{color: '#0D47A1', fontSize: 20, fontWeight: 'bold'}}>Send it!</Text>
+        <Text style={{ color: '#0D47A1', fontSize: 20, fontWeight: 'bold' }}>Add A Destination</Text>
       </TouchableOpacity>
     </ScrollView>
   )
 }
 
-{/* <TouchableOpacity onPress={() => navigation.navigate('Calendar')}>
-  <Image
-    style={{ height: 60, width: 60 }}
-    source={{ uri: 'https://img.icons8.com/plasticine/200/000000/calendar.png' }}
-  />
-  <Text>how long with you stay?</Text>
-</TouchableOpacity> */}
-
-{/* <Text style={styles.label}>Destination</Text>
-  <View style={styles.inputContainer}>
-    <TextInput style={styles.input} />
-  </View> */}
-
-
 const styles = StyleSheet.create({
-    container: {
-      backgroundColor: '#1E88E5',
-      height: '100%',
-      alignItems: 'center',
-      padding: 15
-    },
-    inputContainer: {
-      width: '100%',
-      alignItems: 'center',
-      backgroundColor: '#fff',
-      marginBottom: 20,
-      marginTop: 5,
-      borderRadius: 5,
-      height: 70
-    },
-    label: {
-      color: '#fff',
-      fontSize: 20
-    },
-    input: {
-      justifyContent: 'center',
-      height: '100%',
-      width: '100%',
-      fontSize: 20,
-      paddingHorizontal: 20
-    },
-    addDestination: {
-      backgroundColor: '#2CCB70',
-      width: '100%',
-      height: 50,
-      borderRadius: 5,
-      alignItems: 'center',
-      justifyContent: 'center',
-      marginTop: 15
-    },
-    submitDestination: {
-      backgroundColor: '#FDD835',
-      width: '100%',
-      height: 50,
-      borderRadius: 5,
-      alignItems: 'center',
-      justifyContent: 'center',
-      marginTop: 15
-    }
-  })
+  container: {
+    backgroundColor: '#1E88E5',
+    height: '100%',
+    alignItems: 'center',
+    padding: 15
+  },
+  inputContainer: {
+    width: '100%',
+    alignItems: 'center',
+    backgroundColor: '#fff',
+    marginBottom: 20,
+    marginTop: 5,
+    borderRadius: 5,
+    height: 70
+  },
+  label: {
+    color: '#fff',
+    fontSize: 20
+  },
+  input: {
+    justifyContent: 'center',
+    height: '100%',
+    width: '100%',
+    fontSize: 20,
+    paddingHorizontal: 20
+  },
+  addDestination: {
+    backgroundColor: '#2CCB70',
+    width: '100%',
+    height: 50,
+    borderRadius: 5,
+    alignItems: 'center',
+    justifyContent: 'center',
+    marginTop: 15
+  },
+  submitDestination: {
+    backgroundColor: '#FDD835',
+    width: '100%',
+    height: 50,
+    borderRadius: 5,
+    alignItems: 'center',
+    justifyContent: 'center',
+    marginTop: 15
+  }
+})
 
 
 export default CreateTrip;
