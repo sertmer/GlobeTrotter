497ef6d4e65a5486ee371ffd7749d65b5b1bdf4e
Alan Turing <austeyd@gmail.com>
2020-02-23 15:03:10 -0700

    Add 'query:' to all mutations in apiCalls

--- apiCalls.js
+++ apiCalls.js
@@ -23,8 +23,9 @@ export const getAllTrips = () => {
 };
 
 export const createNewTrip = (name, origin) => {
-  const mutation =   {
-    "mutation": `{createTrip(userApiKey: \"b9aead4b955bccb5c57ef830580f3de5\", name: \"${name}\", origin: \"${origin}\") {trip {id name origin originAbbrev originLat originLong}}}`
+  //Backticks amend the need for backslashes
+  const mutation = {
+    "query": `mutation {createTrip(userApiKey: \"b9aead4b955bccb5c57ef830580f3de5\", name: \"${name}\", origin: \"${origin}\") {trip {id name origin originAbbrev originLat originLong}}}`
   };
 
   const options = {
@@ -46,7 +47,7 @@ export const createNewTrip = (name, origin) => {
 
 export const createNewDestination = (tripId, location, startDate, endDate) => {
   const mutation =   {
-    "mutation": `{createDestination(userApiKey: \"b9aead4b955bccb5c57ef830580f3de5\", tripId: \"${tripId}\", location: \"${location}\", startDate: \"${startDate}\", endDate: \"${endDate}\") {trip {id name origin originAbbrev originLat originLong tripdestinationSet {destination {location abbrev lat long} startDate endDate activitySet {name date category}}}}}`
+    "query": `mutation {createDestination(userApiKey: \"b9aead4b955bccb5c57ef830580f3de5\", tripId: \"${tripId}\", location: \"${location}\", startDate: \"${startDate}\", endDate: \"${endDate}\") {trip {id name origin originAbbrev originLat originLong tripdestinationSet {destination {location abbrev lat long} startDate endDate activitySet {name date category}}}}}`
   };
 
   const options = {
@@ -67,9 +68,32 @@ export const createNewDestination = (tripId, location, startDate, endDate) => {
 };
 
 // export const addActivity = () => {
-//   const mutation =   {
-//     "mutation": "{addActivity(userApiKey: \"b9aead4b955bccb5c57ef830580f3de5\", name: __, origin: __) {trip {id name origin originAbbrev originLat originLong}}}}"
-//   };
+// const mutation =   {
+//   "query": `mutation {createDestination(userApiKey: \"b9aead4b955bccb5c57ef830580f3de5\", tripId: \"${tripId}\", location: \"${location}\", startDate: \"${startDate}\", endDate: \"${endDate}\") {trip {id name origin originAbbrev originLat originLong tripdestinationSet {destination {location abbrev lat long} startDate endDate activitySet {name date category}}}}}`
+// };
+//
+//
+//   const options = {
+//     method: 'POST',
+//     body: JSON.stringify(mutation),
+//     headers: {
+//       'Content-Type': 'application/json',
+//     }
+//   }
+//
+//   return fetch('https://globe-trotter-api.herokuapp.com/graphql/', options)
+//     .then(response => {
+//       if (!response.ok) {
+//         throw Error('error posting new trip')
+//       }
+//       return response.json()
+//     })
+// };
+
+// export const deleteTrip = () => {
+  // const mutation =   {
+  //   "query": `mutation {createDestination(userApiKey: \"b9aead4b955bccb5c57ef830580f3de5\", tripId: \"${tripId}\", location: \"${location}\", startDate: \"${startDate}\", endDate: \"${endDate}\") {trip {id name origin originAbbrev originLat originLong tripdestinationSet {destination {location abbrev lat long} startDate endDate activitySet {name date category}}}}}`
+  // };
 //
 //
 //   const options = {
