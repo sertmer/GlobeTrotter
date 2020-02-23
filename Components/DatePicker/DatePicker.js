71ccca7f8d2eb55d10c94fe085adab746503ade7
sertmer <scottertmer@gmail.com>
2020-02-23 15:00:13 -0700

    Pass setDestionationDates to DatePicker

--- Components/DatePicker/DatePicker.js
+++ Components/DatePicker/DatePicker.js
@@ -2,22 +2,26 @@ import React, { useState } from 'react';
 import { Calendar } from 'react-native-calendars';
 import moment from 'moment';
 
-export const DatePicker = () => {
-  const [ markedDates, setMarkedDates ] = useState({});
-  const [ startPicked, setStartPicked ] = useState(false);
-  const [ endPicked, setEndPicked ] = useState(false);
-  const [ startDate, setStartDate ] = useState('');
-  const [ endDate, setEndDate ] = useState('');
+export const DatePicker = ({ route }) => {
+  const { setDestinationStartDate, setDestinationEndDate } = route.params;
+  console.log('setStart:', setDestinationStartDate)
+  const [markedDates, setMarkedDates] = useState({});
+  const [startPicked, setStartPicked] = useState(false);
+  const [endPicked, setEndPicked] = useState(false);
+  const [startDate, setStartDate] = useState('');
+  const [endDate, setEndDate] = useState('');
 
   const onDayPress = day => {
     if (startPicked === false) {
       let datesMarked = {};
-      datesMarked[day.dateString] = { startingDay: true, color: '#00B0BF', textColor: '#FFFFFF'};
+      datesMarked[day.dateString] = { startingDay: true, color: '#00B0BF', textColor: '#FFFFFF' };
       setMarkedDates(datesMarked);
       setStartPicked(true);
-      setStartDate(day.dateString)
+      setStartDate(day.dateString);
+      setDestinationStartDate(day.dateString);
     } else {
       setEndDate(day.dateString);
+      setDestinationEndDate(day.dateString);
       let datesMarked = markedDates;
       let start = moment(startDate)
       let end = moment(day.dateString)
@@ -27,7 +31,7 @@ export const DatePicker = () => {
         for (let i = 1; i <= range; i++) {
           let tempDate = start.add(1, 'day');
           tempDate = moment(tempDate).format('YYYY-MM-DD')
-          if ( i < range ) {
+          if (i < range) {
             datesMarked[tempDate] = { color: '#00B0BF', textColor: '#FFFFFF' };
           } else {
             datesMarked[tempDate] = { endingDay: true, color: '#00B0BF', textColor: '#FFFFFF' };
