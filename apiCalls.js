75c682137058cd0229f29e959966825220f21caf
Alan Turing <austeyd@gmail.com>
2020-02-23 12:59:44 -0700

    Add fetch call for createNewDestination

--- apiCalls.js
+++ apiCalls.js
@@ -2,7 +2,7 @@ import "isomorphic-fetch";
 
 export const getAllTrips = () => {
   const query = {
-      "query": "{allTrips(userApiKey: \"b9aead4b955bccb5c57ef830580f3de5\") {id name origin originAbbrev originLat originLong tripdestinationSet {destination {location abbrev} startDate endDate activitySet {name date category}}}}"
+      "query": "{allTrips(userApiKey: \"b9aead4b955bccb5c57ef830580f3de5\") {id name origin originAbbrev originLat originLong tripdestinationSet {destination {location abbrev lat long} startDate endDate activitySet {name date category}}}}"
     };
 
   const options = {
@@ -27,6 +27,28 @@ export const createNewTrip = (name, origin) => {
     "mutation": `{createTrip(userApiKey: \"b9aead4b955bccb5c57ef830580f3de5\", name: ${name}, origin: ${origin}) {trip {id name origin originAbbrev originLat originLong}}}}`
   };
 
+  const options = {
+    method: 'POST',
+    body: JSON.stringify(mutation),
+    headers: {
+      'Content-Type': 'application/json',
+    }
+  };
+
+  return fetch('https://globe-trotter-api.herokuapp.com/graphql/', options)
+    .then(response => {
+      if (!response.ok) {
+        throw Error('error posting new trip')
+      }
+      return response.json()
+    })
+};
+
+export const createNewDestination = (tripId, location, startDate, endDate) => {
+  const mutation =   {
+    "mutation": `{createDestination(userApiKey: \"b9aead4b955bccb5c57ef830580f3de5\", tripId: ${tripId}, location: ${location}, startDate: ${startDate}, endDate: ${endDate}) {trip {id name origin originAbbrev originLat originLong tripdestinationSet {destination {location abbrev lat long} startDate endDate activitySet {name date category}}}}}`
+  };
+
 
   const options = {
     method: 'POST',
@@ -39,34 +61,11 @@ export const createNewTrip = (name, origin) => {
   return fetch('https://globe-trotter-api.herokuapp.com/graphql/', options)
     .then(response => {
       if (!response.ok) {
-        throw Error('error posting new trip')
+        throw Error('error posting new destination data')
       }
       return response.json()
     })
 };
-
-// export const createNewDestination = () => {
-//   const mutation =   {
-//     "mutation": "{createDestination(userApiKey: \"b9aead4b955bccb5c57ef830580f3de5\", name: __, origin: __) {trip {id name origin originAbbrev originLat originLong}}}}"
-//   };
-//
-//
-//   const options = {
-//     method: 'POST',
-//     body: JSON.stringify(mutation),
-//     headers: {
-//       'Content-Type': 'application/json',
-//     }
-//   }
-//
-//   return fetch('https://globe-trotter-api.herokuapp.com/graphql/', options)
-//     .then(response => {
-//       if (!response.ok) {
-//         throw Error('error posting new trip')
-//       }
-//       return response.json()
-//     })
-// };
 
 // export const addActivity = () => {
 //   const mutation =   {
